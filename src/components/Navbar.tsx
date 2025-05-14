a5f8e31b7ffec42777a9f852ab513bf98397abad
sajid-islam <sajidislam941@gmail.com>
2025-05-14 20:36:56 +0600

    create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,18 @@
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <nav className="bg-[#051923] px-4 py-4 shadow-md">
+            <div className="flex items-center justify-center gap-2">
+                <img
+                    src="/src/assets/trapurl-logo.png"
+                    alt="site logo"
+                    className="size-9"
+                />
+                <h1 className="text-white text-2xl font-bold">
+                    TRAP<span className="text-[#56cbf9]">U</span>RL
+                </h1>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
